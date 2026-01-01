a76cf1e319f6a1ea88d37cd8602505c7ae96145d
TIlukhina <Tatiana.Ilukhina@advlab.io>
2026-01-01 22:01:52 +0300

    Настроен файл docker-compose для запуска приложения в контейнере

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import dotenv
 dotenv.load_dotenv()
 import json
@@ -6,12 +8,20 @@ from app.database.engine import create_db_and_tables
 from fastapi import FastAPI, HTTPException
 from fastapi_pagination import Page, add_pagination, paginate
 from app.models.AppStatus import AppStatus
-from routers import status, users
+from app.routers import status, users
 from app.models.User import User
 # from database import users_db
+import uvicorn
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("On startup")
+    create_db_and_tables()
+    yield
+    print("On shutdown")
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
@@ -20,6 +30,4 @@ add_pagination(app)
 
 
 if __name__ == "__main__":
-    import uvicorn
-    create_db_and_tables()
     uvicorn.run(app, host="127.0.0.1", port=8000)
